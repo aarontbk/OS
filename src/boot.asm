00d32ba92d0a9f6af881abd0eb345f3156835fb1
aaron <aarontebeka@gmail.com>
2025-04-20 18:12:14 +0300

    Bootloader is now working. started stack and loaded kernel into memory. switched to protected mode.

--- src/boot.asm
+++ src/boot.asm
@@ -4,69 +4,93 @@
 CODE_OFFSET equ 0x8
 DATA_OFFSET equ 0x10
 
+KERNEL_LOAD_ADDRESS equ 0x10000      ; physical
+KERNEL_START_ADDRESS equ 0x100000    ; where we copy to in PM
+
 start:
     cli
-    mov ax, 0x00
+    xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov sp, 0x7C00
-    sti
 
-load_PM:
-    cli
-    lgdt[gdt_descriptor]
-    mov eax, cr0
-    or al, 1
-    mov cr0, eax
+mov ax, 0x1000 ; segment = 0x1000
+mov es, ax
 
-    jmp CODE_OFFSET:PModeMain
+; Load kernel (8 sectors from disk)
+mov ah, 0x02
+mov al, 8 ; sectors
+mov ch, 0
+mov cl, 2 ; sector 2 onward
+mov dh, 0
+mov dl, 0x80 ; hdd
 
-; GDT implementation
+xor bx, bx ; offset = 0
+int 0x13
+jc load_error
+
+; Enable PM
+lgdt [gdt_descriptor]
+mov eax, cr0
+or eax, 1
+mov cr0, eax
+jmp CODE_OFFSET:PModeMain
+
+; GDT
 gdt_start:
-    gdt_null:
-        dw 0, 0
-        db 0, 0, 0, 0, 0, 0, 0, 0
+gdt_null:      dq 0
 
-    gdt_code:
-        dw 0xFFFF ; limit
-        dw 0x0000 ; base low
-        db 0x00 ; base middle
-        db 10011010b
-        db 11001111b ; granularity
-        db 0x00 ; base high
+gdt_code:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
 
-    gdt_data:
-        dw 0xFFFF ; limit
-        dw 0x0000 ; base low
-        db 0x00 ; base middle
-        db 10010010b
-        db 11001111b ; granularity
-        db 0x00 ; base high
+gdt_stack:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10010110b
+    db 11001111b
+    db 0x00
 
 gdt_end:
 
 gdt_descriptor:
-    dw gdt_end - gdt_start - 1 ; size of GDT - 1
-    dd gdt_start ; address of GDT
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
 
 [BITS 32]
 PModeMain:
-    mov ax, DATA_OFFSET
+    mov ax, CODE_OFFSET
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
+    mov ax, DATA_OFFSET
     mov ss, ax
     mov ebp, 0xAC00
     mov esp, ebp
 
+    ; Enable A20 (optional if already done)
     in al, 0x92
     or al, 2
     out 0x92, al
 
+    ; Copy kernel from 0x10000 → 0x100000
+    mov esi, KERNEL_LOAD_ADDRESS
+    mov edi, KERNEL_START_ADDRESS
+    mov ecx, 512
+    cld
+    rep movsd
+
+    jmp CODE_OFFSET:KERNEL_START_ADDRESS
+
+load_error:
     jmp $
 
-
-times 510 - ($-$$) db 0
+times 510 - ($ - $$) db 0
 dw 0xAA55
